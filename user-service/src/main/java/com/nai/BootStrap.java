6ace9471ddc55512eebad9c312e06ab35d07a4fa
TheNai <69084340+xny838754132@users.noreply.github.com>
2020-12-08 11:54:47 +0800

    first commit

---
+++ user-service/src/main/java/com/nai/BootStrap.java
@@ -0,0 +1,11 @@
+package com.nai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(BootStrap.class, args);
+    }
+}
